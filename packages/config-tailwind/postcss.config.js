f352b48c127a52b95ee30c8392e0387a38dcf7f0
akashdeep023 <ad3500476@gmail.com>
2025-09-07 13:30:00 +0530

    fix: reinitialise tailwindcss v4 and Update

--- packages/config-tailwind/postcss.config.js
+++ packages/config-tailwind/postcss.config.js
@@ -1,6 +1,6 @@
-// Optional PostCSS configuration for applications that need it
-export const postcssConfig = {
+module.exports = {
 	plugins: {
 		"@tailwindcss/postcss": {},
+		autoprefixer: {},
 	},
 };
