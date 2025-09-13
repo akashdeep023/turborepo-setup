baad492e1b88e6ccb779c9c01b5483809ad54fa9
akashdeep023 <ad3500476@gmail.com>
2025-09-14 02:24:58 +0530

    add padding

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className="w-full">
 				<Header />
-				<main className="flex flex-col items-center justify-between min-h-[80vh]">
+				<main className="flex flex-col items-center justify-between min-h-[80vh] pb-4 px-4 md:pb-8 md:px-8">
 					{children}
 				</main>
 				<Footer />
